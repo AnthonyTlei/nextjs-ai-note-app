a4c387f260c72385189f30ce5d591a15b3e97140
Anthony Tleiji <anthonytleiji@gmail.com>
2023-11-04 17:58:34 +0200

    Notes: - Add Note component - Display notes in notes page

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,4 @@
+import Note from "@/components/Note";
 import prisma from "@/lib/db/prisma";
 import { auth } from "@clerk/nextjs";
 
@@ -14,5 +15,12 @@ export default async function NotesPage() {
     where: { userId },
   });
 
-  return <div>{JSON.stringify(allNotes)}</div>;
+  return (
+    <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
+      {allNotes.map((note) => (
+        <Note key={note.id} note={note} />
+      ))}
+      {allNotes.length === 0 && <div className="col-span-full text-center">{"You don't have any notes yet."}</div>}
+    </div>
+  );
 }
