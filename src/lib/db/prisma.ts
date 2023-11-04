599de81fff3ea11cecbe58bf5e0d5b5432b7570a
Anthony Tleiji <anthonytleiji@gmail.com>
2023-11-04 14:37:33 +0200

    Setup Prisma & MongoDB: - Configured Prisma - Added Note Schema - Added Singleton Prisma client

---
+++ src/lib/db/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  const prisma = new PrismaClient();
+  return prisma;
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
